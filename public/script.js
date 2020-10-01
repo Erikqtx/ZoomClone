f554b164e5d6399bce05756390747b2653783124
Erikqtx <glaser-erik@web.de>
2020-10-01 14:20:54 +0200

    Adding User ID and starting Peer to Peer

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+const socket = io("/");
+
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 myVideo.muted = true;
@@ -12,6 +14,16 @@ navigator.mediaDevices
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
   });
+
+socket.emit("join-room", ROOM_ID);
+
+socket.on("user-connected", () => {
+  connecToNewUser();
+});
+
+const connecToNewUser = () => {
+  console.log("new user");
+};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+const { Socket } = require("dgram");
 const express = require("express");
 const { allowedNodeEnvironmentFlags } = require("process");
 const app = express();
 const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const { v4: uuidv4 } = require("uuid");
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -12,5 +18,12 @@ app.get("/", (req, res) => {
 
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected");
+  });
 });
 server.listen(3030);
