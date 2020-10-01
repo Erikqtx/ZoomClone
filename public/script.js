3691d44c2cc33ef98620cb2f25b9cd609005d228
Erikqtx <glaser-erik@web.de>
2020-10-01 13:44:33 +0200

    Finishing own Video

--- public/script.js
+++ public/script.js
@@ -1,3 +1,8 @@
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+let myVideoStream;
 navigator.mediaDevices
   .getUserMedia({
     video: true,
@@ -5,8 +10,13 @@ navigator.mediaDevices
   })
   .then((stream) => {
     myVideoStream = stream;
+    addVideoStream(myVideo, stream);
   });
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
 };
