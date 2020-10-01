7fa4b779e9c9aaaee40c9192c1feae9cffe40573
Erikqtx <glaser-erik@web.de>
2020-10-01 13:35:27 +0200

    Adding Stream

---
+++ public/script.js
@@ -0,0 +1,12 @@
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+  });
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+};
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const { allowedNodeEnvironmentFlags } = require("process");
 const app = express();
 const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.status(200).write("Hello Word");
+  res.redirect(`/${uuidv4()}`);
 });
 
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 server.listen(3030);
